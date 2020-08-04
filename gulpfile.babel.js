cb1a22c776189798dcdc716516779584018b384e
Anılcan <igoodie@programmer.net>
2020-08-04 04:02:19 +0300

    feat(core): initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,52 @@
+import * as fs from "fs";
+import * as childProcess from "child_process";
+import * as pkg from "pkg";
+import gulp from "gulp";
+import babel from "gulp-babel";
+import typescript from "gulp-typescript";
+import nodemon from "gulp-nodemon";
+
+const packageJSON = JSON.parse(fs.readFileSync("package.json"));
+const tsProject = typescript.createProject("tsconfig.json");
+
+function clear(done) {
+  const buildExists = fs.existsSync("./build");
+  const distExists = fs.existsSync("./dist");
+  if (buildExists) childProcess.execSync("rmdir /q /s build");
+  if (distExists) childProcess.execSync("rmdir /q /s dist");
+  return done();
+}
+
+function transpile() {
+  return gulp
+    .src(["src/**/*.ts"])
+    .pipe(gulp.src("types/**/*.ts"))
+    .pipe(tsProject())
+    .pipe(gulp.src("src/**/*.js"))
+    .pipe(babel())
+    .pipe(gulp.dest(packageJSON.gulp.buildPath));
+}
+
+function bundle() {
+  return pkg.exec([
+    packageJSON.gulp.builtMain,
+    ...(packageJSON.gulp?.buildPlatforms
+      ? ["--targets", packageJSON.gulp?.buildPlatforms.join(",")]
+      : []),
+    "--out-path",
+    packageJSON.gulp.distPath,
+  ]);
+}
+
+function start() {
+  return nodemon({
+    script: packageJSON.main,
+    ext: packageJSON.gulp.nodemonExt.join(" "),
+    tasks: ["transpile"],
+  });
+}
+
+exports.default = gulp.series(clear, transpile, bundle);
+exports.build = gulp.series(clear, transpile);
+exports.clear = clear;
+exports.start = start;
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+// globalThis.sampleVariable = "Foo bar baz";
+
+const a: Foo = {
+  bar: "Foo bar baz3",
+};
+
+console.log("Hello world!", a);
